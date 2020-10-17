a027e321662a43476cc0f49f6d8bcc8735d100e7
xvedar <irezki@gmail.com>
2020-10-17 21:26:54 +0200

    git is first then vim

--- ubuntu.install.utils.sh
+++ ubuntu.install.utils.sh
@@ -2,7 +2,7 @@
 sudo apt-get update
 sudo apt install openssh-server
 sudo systemctl status ssh
+sudo apt install git
 sudo apt install vim
 sudo apt-get install libxcb-xinerama0
 sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
-sudo apt install git
