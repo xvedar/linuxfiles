c0acc26152fa6f351dfc30401901a791380dc365
xvedar <irezki@gmail.com>
2022-05-01 12:19:04 +0200

    added some mount updates

--- ubuntu.install.utils.sh
+++ ubuntu.install.utils.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 sudo apt-get update
+# ssh
 sudo apt install openssh-server
 sudo systemctl status ssh
+
+# git + vim
 sudo apt install git
 sudo apt install vim
+
+# calibre
 sudo apt-get install libxcb-xinerama0
 sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
+
+# utils to mount and manage our network shares
+sudo apt install cifs-utils
+
+sudo mkdir -p /share2/Download
+sudo mkdir -p /share2/Multimedia
+# uid/gid is available via 'id' command
+sudo mount.cifs //192.168.1.12/Download   /share2/Download   -o user=admin,pass=@2737594,uid=59999,gid=59999,nobrl
+sudo mount.cifs //192.168.1.12/Multimedia /share2/Multimedia -o user=admin,pass=@2737594,uid=59999,gid=59999,nobrl
